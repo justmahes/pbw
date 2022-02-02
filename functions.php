9add3b835b8d4ae18240ed6bd6ef938efcf3debb
siwa <siwarjsa@gmail.com>
2022-02-02 20:22:13 +0800

    minor update

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// read data
+function readdata($result)
+{
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
--- login/index.php
+++ login/index.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
-<!-- Coding By CodingNepal - youtube.com/codingnepal -->
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    <div class="center">
-      <h1>Login</h1>
-      <form method="post">
-        <div class="txt_field">
-          <input type="text" required>
-          <span></span>
-          <label>Username</label>
-        </div>
-        <div class="txt_field">
-          <input type="password" required>
-          <span></span>
-          <label>Password</label>
-        </div>
-        <input type="submit" value="Login">
-        <div class="signup_link">
-        </div>
-        <div class="pass">Forgot Password?</div>
-      </form>
-    </div>
 
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="center">
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+      <div class="txt_field">
+        <input type="text" name="username" required>
+        <span></span>
+        <label>Username</label>
+      </div>
+      <div class="txt_field">
+        <input type="password" name="password" required>
+        <span></span>
+        <label>Password</label>
+      </div>
+      <input type="submit" name="masuk" value="Login">
+      <div class="signup_link">
+      </div>
+      <div class="pass">Forgot Password?</div>
+    </form>
+  </div>
+
+</body>
+
 </html>
